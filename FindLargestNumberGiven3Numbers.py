003486a25b07baddf7175e4dc5efd4d8673bff88
Rushan <raushansinghrajpoot687@gmail.com>
2025-03-16 19:35:31 +0530

    added file

---
+++ FindLargestNumberGiven3Numbers.py
@@ -0,0 +1,9 @@
+a = int(input("enter any 3 numbers\n"))
+b =int(input())
+c = int(input())
+if(a>b and a>c):
+    print("first number is largest")
+elif(b>a and b>c):
+    print("second number is largest")
+else:
+    print("third number is largst")
