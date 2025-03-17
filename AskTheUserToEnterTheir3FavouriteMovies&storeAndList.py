3c120c8e2f24912c76bdf2c6bde09dc087e5dfc4
Rushan <raushansinghrajpoot687@gmail.com>
2025-03-17 19:51:59 +0530

    added file AskTheUserToEnterTheir3FavouriteMovies&storeAndList.py

---
+++ AskTheUserToEnterTheir3FavouriteMovies&storeAndList.py
@@ -0,0 +1,5 @@
+n = input("rnter your 1st movie name :")
+n2 =input("enter your 2nd movie name :")
+n3 = input("enter your 3rd movie name :")
+movies = [n,n2,n3]
+print("you enterd your Favourite movies",movies)
