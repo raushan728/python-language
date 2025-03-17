bdf18558894c6418735abb25c10125e46045be74
Rushan <raushansinghrajpoot687@gmail.com>
2025-03-17 19:40:20 +0530

    added file

---
+++ StringMethod.py
@@ -0,0 +1,32 @@
+name = "I am Raushan"
+name2 ="Raushan"
+name3 = "Kumar"
+# print("this is added"+" " +name2+" " +name3)
+
+# print length of String
+# print(len(name))
+
+# indexing
+# print(name[3])
+# print("your index :" + " " + name[3])
+# print(name[1:6])
+# print(name[1:]) #[1:LAST]
+# print(name[:6])  #[0:6]
+
+# backward counting
+#print(name[-9:])
+
+#return true and false
+# print(name.endswith("an"))
+
+# #capitalize ist char
+# print(name.capitalize())
+
+# #replace charctor
+# print(name.replace(" ","_"))
+
+# #return 1st index of 1st occurrer
+# print(name.find("a"))
+
+#count charactor
+print(name.count("a"))
