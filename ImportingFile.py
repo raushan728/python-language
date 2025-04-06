9e38c71d6367196c78f18ecc3210a688e0a80e19
RAUSHAN KUMAR <raushansinghrajpoot687@gmail.com>
2025-04-06 14:59:14 +0530

    Add files via upload

---
+++ ImportingFile.py
@@ -0,0 +1,10 @@
+def greet(name):
+    print(f"Good morning {name}")
+
+
+if __name__ == "__main__":
+    n = input("Enter your name ")  #if you do not it this in that file(Importing file2.py) called all items in this file
+    greet(n)
+
+
+# file name = Importingfile2.py
