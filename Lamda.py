bac130afb01167a0481eafc88cf0e0db51f4e138
RAUSHAN KUMAR <raushansinghrajpoot687@gmail.com>
2025-04-07 14:07:04 +0530

    Add files via upload

---
+++ Lamda.py
@@ -0,0 +1,10 @@
+# def rau(a):
+#     return a + 5
+
+rau = lambda a: a + 5  # same a function we use to create single line function
+square = lambda a: a * a
+sum = lambda a, b, c: a + b + c
+x = 3
+print(rau(x))
+print(square(x))
+print(sum(x, 3, 5))
