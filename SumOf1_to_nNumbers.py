1877b804d8dfabead2306fb69bae05225dedc54d
Rushan <raushansinghrajpoot687@gmail.com>
2025-05-31 12:44:06 +0530

    Basic Input/Output and Variables

--- SumOf1_to_nNumbers.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Enter your number"))
-sum =0
-for i in range(1,n+1):
-    sum+=i
-print("your sum",sum)
--- SumOf1tonNumbersUsingRecursion.py
+++
@@ -1,8 +0,0 @@
-def sum(n):
-    if n == 1:
-        return 1
-    return sum(n-1)+n
-
-
-a = int(input("Enter a number :"))
-print("Sum is", sum(a))
