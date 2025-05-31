f74d4ea388cadbe10842e8e398a7d0d26fc31939
RAUSHAN KUMAR <raushansinghrajpoot687@gmail.com>
2025-05-31 12:53:05 +0530

    Delete 04_AvgOf3NumbersUsingFunction.py

--- 04_AvgOf3NumbersUsingFunction.py
+++
@@ -1,8 +0,0 @@
-def avg(a, b, c):
-    return (a + b + c) / 3
-
-
-num = int(input("Enter first number :"))
-num2 = int(input("Enter second number :"))
-num3 = int(input("Enter third number :"))
-print("Avg is", avg(num, num2, num3))
